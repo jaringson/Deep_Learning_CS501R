cddc372183d89c4b783620e7f16d79a6dc1db64f
Jaron <jaringson@gmail.com>
2017-10-11 15:36:29 -0600

    Ending lab5

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -22,6 +22,10 @@ for label in labels:
     one_hot.put(label,1)
     one_hot_labels.append(one_hot)
 
+train_f = features[0:8000]
+train_l = one_hot_labels[0:8000]
+test_f = features[8000:10000]
+test_l = one_hot_labels[8000:10000]
 
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.2)
@@ -60,6 +64,8 @@ def fc( x, out_size=50, is_output=False, name="fc" ):
 
 x = tf.placeholder(tf.float32, shape=[None, 32*32*3], name="images")
 label_ = tf.placeholder(tf.float32, shape=[None, 10])
+keep_prob = tf.placeholder(tf.float32)
+
 x_image = tf.reshape(x, [-1,32,32,3])
 
 h0 = conv(x_image)
@@ -70,36 +76,59 @@ shape = h2.get_shape().as_list()
 flat = tf.reshape(h2,[-1,shape[1]*shape[2]*shape[3]])
 
 fc1 = fc(flat)
-fc2 = fc(fc1,out_size=10,is_output=True)
+fc1_drop = tf.nn.dropout(fc1, keep_prob)
+
+fc2 = fc(fc1_drop,out_size=10,is_output=True)
 
 label_conv = tf.nn.softmax(fc2)
 
 
 with tf.name_scope('Cost') as scope:
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(label_ * tf.log(tf.clip_by_value(label_conv,1e-10,1)), axis=[1]))
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(label_ * tf.log(tf.clip_by_value(label_conv,1e-10,1)), reduction_indices=[1]))
     
 with tf.name_scope('Accuracy') as scope:
     correct_prediction = tf.equal(tf.argmax(label_conv,1), tf.argmax(label_,1))
     label_acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
   
 with tf.name_scope('Optimizer') as scope:
-    train_step = tf.train.AdamOptimizer(1e-5).minimize(cross_entropy)
+    train_step = tf.train.AdamOptimizer(3e-6).minimize(cross_entropy)
     
 acc_summary = tf.summary.scalar( 'Accuracy', label_acc )
 cost_summary = tf.summary.scalar( 'Cost', cross_entropy )
 
 merged_summary_op = tf.summary.merge_all()
-summary_writer = tf.summary.FileWriter("./tf_logs",graph=sess.graph)
+summary_writer = tf.summary.FileWriter("./tf_logs2",graph=sess.graph)
 
 sess.run(tf.global_variables_initializer())
 
 NUM_EPOCHS = 1000
-# print features[:2]
-# print one_hot_labels[:2]
+place = 0
+test_place = 0
+
+
+def train_get_batch(size):
+    global place
+    if size*place+50 > 8000-1:
+        place = -1
+	return train_f[0:size],train_l[0:size]
+    place += 1
+    return train_f[size*place:size*place+size], train_l[size*place:size*place+size] 
+
+def test_get_batch(size):
+    global test_place
+    if size*test_place+50 > 2000-1:
+        test_place = 0  
+    test_place += 1
+    return train_f[size*test_place:size*test_place+size], train_l[size*test_place:size*test_place+size] 
 
 for i in range(NUM_EPOCHS):
-    train_step.run(feed_dict={x:features[:10], label_:one_hot_labels[:10]})
-    summary_str,l = sess.run([merged_summary_op, label_acc],feed_dict={x:features[:10], label_:one_hot_labels[:10]})
+    while place != -1:	
+        batch = train_get_batch(50)
+        train_step.run(feed_dict={x:batch[0], label_:batch[1], keep_prob:.5})
+    place = 0 
+    test_batch = test_get_batch(100)
+    summary_str,l = sess.run([merged_summary_op, label_acc],feed_dict={x:test_batch[0], label_:test_batch[1], keep_prob:.5})
     
     print("%d, %g"%(i,l))
     summary_writer.add_summary(summary_str,i)
+summary_writer.close()
