f903c6d29fbff011dc18789507cbec1ea373e28e
jaron <jaringson@gmail.com>
2017-11-14 17:57:37 -0700

    Changes to ops for style and content loss

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -34,9 +34,15 @@ ops = [ getattr( vgg, x ) for x in layers ]
 content_acts = sess.run( ops, feed_dict={vgg.imgs: content_img } )
 style_acts = sess.run( ops, feed_dict={vgg.imgs: style_img} )
 
-content_layer = ops[8]
-# print content_loss.get_shape()
+# print content_acts
+# print vgg
 
+content_layer = content_acts[8]
+print content_layer.shape
+
+orig_content = ops[8]
+
+print orig_content.get_shape()
 # content_loss = tf.reshape(content_loss, [1, 28*28, 512])
 # print content_loss.get_shape()
 
@@ -51,7 +57,7 @@ content_layer = ops[8]
 
 # print style_acts
 
-content_loss = 0.5 * tf.reduce_sum(tf.square(tf.subtract(content_layer - opt_img)))
+content_loss = 0.5 * tf.reduce_sum(tf.square(tf.subtract(content_layer, orig_content)))
 #
 # --- construct your cost function here
 #
@@ -64,12 +70,14 @@ content_loss = 0.5 * tf.reduce_sum(tf.square(tf.subtract(content_layer - opt_img
 style_pos = [0,2,4,7,10]
 style_loss = 0
 for pos in style_pos:
-	style_layer = ops[pos]
+	style_layer = style_acts[pos]
+	orig_style = ops[pos]
+	style_layer = tf.constant(style_layer)
 	shape = style_layer.get_shape().as_list()
 	gram = tf.reshape(style_layer, [1,shape[1]*shape[2], shape[3]])
 	gram = tf.reshape(gram, [shape[1]*shape[2], shape[3]])
 	gram = tf.reduce_sum(tf.multiply(gram, gram))
-	style_loss += (1.0/ (4 * shape[3]**2 * (shape[1]*shape[2])**2 ) ) * tf.reduce_sum(tf.square(tf.subtract(gram - opt_img)))
+	style_loss += (1.0/ (4 * shape[3]**2 * (shape[1]*shape[2])**2 ) ) * tf.reduce_sum(tf.square(tf.subtract(gram, style_img)))
 style_loss *= 1.0/5.0
 
 alpha = 1.0
@@ -79,7 +87,7 @@ loss = alpha * content_loss + beta * style_loss
 
 # --- place your adam optimizer call here
 #     (don't forget to optimize only the opt_img variable)
-adam = tf.train.AdamOptimizer(0.1).minimize(loss)
+adam = tf.train.AdamOptimizer(0.1).minimize(loss, var_list=[opt_img])
 
 # this clobbers all VGG variables, but we need it to initialize the
 # adam stuff, so we reload all of the weights...
@@ -91,5 +99,5 @@ sess.run( opt_img.assign( content_img ))
 
 # --- place your optimization loop here
 for i in range(100):
-	r_img, l, cl, sl = adam.run([opt_img, loss, content_loss, style_loss], feed_dict={})
+	optimze, r_img, l, cl, sl = sess.run([adam, opt_img, loss, content_loss, style_loss])
 	print i, l, sl, cl
